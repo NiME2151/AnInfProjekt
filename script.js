659a49c3fefaba71c2d97d2e82a68839bd22dfb7
NiME2151 <lordlol134@gmail.com>
2020-01-24 12:08:34 +0100

    map

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 $(document).ready(function () {
+
+    $("section div").keypress(function() {
+        if (e.keyCode == 37) {
+            $("section div").animate({
+                left: '-=25'
+            }, 200, function() {
+            });
+        }
+    });
+
+    /*
     $("section").click(function () {
+
         $(this).hide();
     })
+    */
+
+    const map = [
+        '###########',
+        '#         #',
+        '#         #',
+        '#         #',
+        '#         #',
+        '###########'
+    ];
+
+    let signs = {
+        '#':  'wall',
+        ' ': 'floor'
+    }
+
+
+    map.forEach(line => {
+        line.split('').forEach(sign => {
+            $("section").append(`<div class='${signs[sign]}'></div>`)
+        });
+    });
+
 })
 

235f691c53ec231b878c5da41979da1d44e4c198
fabiiiii <fabianhuge@gmx.de>
2020-01-31 13:59:42 +0100

    health leiste und so

---
+++ player.js
@@ -0,0 +1,8 @@
+export class Player {
+
+    progHealth = document.getElementById("health").val();
+
+
+    //click(function(){
+
+}
--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 $(document).ready(function () {
 
-    $(document).keydown(function(e) { //ArrowDown
+    $('#health').click(function () {
+        oldVal = $('#health').val();
+        newVal = oldVal - 10;
+        $('#health').val(newVal);
+    });
+
+    $(document).keydown(function (e) { //ArrowDown
         if (e.which == 40) {
             $("div").animate({
                 bottom: '-=25'
-            }, 200, function() {
+            }, 200, function () {
             });
         }
     });
-    $(document).keydown(function(e) { //ArrowUp
+    $(document).keydown(function (e) { //ArrowUp
         switch (e.which) {
             case 39:
                 $("section div").animate({
                     bottom: '-=25'
-                }, 200, function() {
+                }, 200, function () {
                 });
                 break;
         }
     });
-    $(document).keypress(function() { //ArrowLeft
+    $(document).keypress(function () { //ArrowLeft
         if (e.which == 37) {
             $("div").animate({
                 left: '-=25'
-            }, 200, function() {
+            }, 200, function () {
             });
         }
     });
-    $(document).keypress(function() { //ArrowRight
+    $(document).keypress(function () { //ArrowRight
         if (e.which == 39) {
             $("div").animate({
                 left: '-=25'
-            }, 200, function() {
+            }, 200, function () {
             });
         }
     });

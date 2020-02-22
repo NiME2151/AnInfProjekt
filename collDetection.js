7e0fc6e6945e20570fda98f5661374d22949d672
fabiiiii <fabianhuge@gmx.de>
2020-02-22 17:18:05 +0100

    gegner sterben fertig

--- collDetection.js
+++ collDetection.js
@@ -17,46 +17,61 @@ function healthDecrease() {
 
 
 const speed = 100;
-function recthit(rectone, player, direction){
-
-    var r1 = $(rectone);
+function recthit(rectone, player, direction) {
+    let coll = false;
+    // var r1 = $(rectone);
     var r2 = $(player);
+    $(".enemy").each(function (index) {
+        // console.table($(this).offset().left);
 
-    var r1x = r1.offset().left;
-    var r1w = r1.width()-1;
-    var r1y = r1.offset().top;
-    var r1h = r1.height()-1;
+        var r1x = $(this).offset().left;
+        var r1w = $(this).width() - 1;
+        var r1y = $(this).offset().top;
+        var r1h = $(this).height() - 1;
 
-    var r2x = r2.offset().left;
-    var r2w = r2.width()-1;
-    var r2y = r2.offset().top;
-    var r2h = r2.height()-1;
+        var r2x = r2.offset().left;
+        var r2w = r2.width() - 1;
+        var r2y = r2.offset().top;
+        var r2h = r2.height() - 1;
 
-    if(direction === 'down') {
-        r2y+=speed;
-    }
-    if (direction === 'up') {
-        r2y-=speed;
-    }
-    if (direction === 'left') {
-        r2x-=speed;
-    }
-    if (direction === 'right') {
-        r2x+=speed;
-    }
+        // if (direction === 'down') {
+        //     r2y += speed;
+        // }
+        // if (direction === 'up') {
+        //     r2y -= speed;
+        // }
+        // if (direction === 'left') {
+        //     r2x -= speed;
+        // }
+        // if (direction === 'right') {
+        //     r2x += speed;
+        // }
 
-    if( r1y+r1h < r2y ||
-        r1y > r2y+r2h ||
-        r1x > r2x+r2w ||
-        r1x+r1w < r2x ){
-        return false;
-    }else{
-        // If health === 0 game restarts
-        if (healthDecrease() === 0) {
-            alert("GAME OVER")
-            location.reload(true);
+        if (r1y + r1h < r2y ||
+            r1y > r2y + r2h ||
+            r1x > r2x + r2w ||
+            r1x + r1w < r2x) {
+            coll = false;
+        } else {
+            if (healthDecrease() === 0) {
+                alert("GAME OVER")
+                location.reload(true);
+            }
+            else if ($(this).attr('id') === '66') {
+                $(this).attr("class", "floor")
+                healthDecrease();
+            }
+            else if ($(this).attr('id') === '106') {
+                $(this).attr("class", "floor")
+                healthDecrease();
+            }
+            else if ($(this).attr('id') === '87') {
+                $(this).attr("class", "floor")
+                healthDecrease();
+            }
+            coll = true;
+            
         }
-        return true;
-    }
-
+    });
+    return coll;
 }
